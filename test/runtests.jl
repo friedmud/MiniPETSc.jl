98593c321f605042cbf0fd9d3fad9a643fb0480b
Derek Gaston <derek.gaston@inl.gov>
2016-12-04 17:02:38 -0500

    Enable the other tests again

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,10 +6,12 @@ using MiniPETSc
 
 using Base.Test
 
-#include("test_Mat.jl")
-#include("test_Vec.jl")
-#include("test_KSP.jl")
-
-include("test_GhostedVec.jl")
+if MPI.Comm_size(MPI.COMM_WORLD) == 1
+    include("test_Mat.jl")
+    include("test_Vec.jl")
+    include("test_KSP.jl")
+else
+    include("test_GhostedVec.jl")
+end
 
 MPI.Finalize()

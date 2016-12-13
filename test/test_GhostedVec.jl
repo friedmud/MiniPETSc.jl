7300eae98590947b02e69166f69d4f1c3f9da07e
Derek Gaston <derek.gaston@inl.gov>
2016-12-13 00:23:13 -0500

    Reenable tests

--- test/test_GhostedVec.jl
+++ test/test_GhostedVec.jl
@@ -1,102 +1,102 @@
 @testset "GhostedVec.jl" begin
     begin
         if MPI.Comm_size(MPI.COMM_WORLD) == 2
-#            if MPI.Comm_rank(MPI.COMM_WORLD) == 0
-#                vec = GhostedPetscVec([6,7], n_local=(Int32)(4))
-#                @test vec.first_local_index == 1
-#                @test vec.last_local_index == 4
-#
-#                @test vec.global_to_local_map == Dict{Int32, Int32}(6 => 5, 7 => 6)
-#
-#                @test vec.sized
-#
-#                # # Test individual assignment
-#                vec[2] = 2.6
-#
-#                assemble!(vec)
-#
-#                @test size(vec) == 8
-#
-#                @test vec[2] == 2.6
-#
-#                @test vec[6] == 1.2
-#
-#                vec[3] = 8.7
-#
-#                assemble!(vec)
-#
-#                @test vec[3] == 8.7
-#                @test vec[7] == 2.9
-#            else
-#                vec = GhostedPetscVec([1,2], n_local=(Int32)(4))
-#
-#                @test vec.first_local_index == 5
-#                @test vec.last_local_index == 8
-#
-#                @test vec.global_to_local_map == Dict{Int32, Int32}(1 => 5, 2 => 6)
-#
-#                @test vec.sized
-#
-#                # # Test individual assignment
-#                vec[6] = 1.2
-#
-#                assemble!(vec)
-#
-#                @test size(vec) == 8
-#
-#                @test vec[6] == 1.2
-#
-#                @test vec[2] == 2.6
-#
-#                vec[7] = 2.9
-#
-#                assemble!(vec)
-#
-#                @test vec[7] == 2.9
-#            end
-#
-#            if MPI.Comm_rank(MPI.COMM_WORLD) == 0
-#                vec = GhostedPetscVec([6,7], n_local=(Int32)(4))
-#
-#                # Test Ranged assignment
-#                vec[1:2] = (Float64)[1 2]
-#
-#                assemble!(vec)
-#
-#                @test vec[1:2] == (Float64)[1,2]
-#
-#                # Test Array access to ghosts
-#                @test vec[[1,6]] == [1,6]
-#            else
-#                vec = GhostedPetscVec([1,2], n_local=(Int32)(4))
-#
-#                # Test Ranged assignment
-#                vec[6:7] = (Float64)[6 7]
-#
-#                assemble!(vec)
-#
-#                @test vec[6:7] == (Float64)[6,7]
-#            end
-#
-#            if MPI.Comm_rank(MPI.COMM_WORLD) == 0
-#                vec = GhostedPetscVec([6,7], n_local=(Int32)(4))
-#
-#                # Test Array assignment
-#                vec[[1,3]] = (Float64)[1 3]
-#
-#                assemble!(vec)
-#
-#                @test vec[[1,3]] == (Float64)[1,3]
-#            else
-#                vec = GhostedPetscVec([1,2], n_local=(Int32)(4))
-#
-#                # Test Array assignment
-#                vec[[6,8]] = (Float64)[6 8]
-#
-#                assemble!(vec)
-#
-#                @test vec[[6,8]] == (Float64)[6,8]
-#            end
+            if MPI.Comm_rank(MPI.COMM_WORLD) == 0
+                vec = GhostedPetscVec([6,7], n_local=(Int32)(4))
+                @test vec.first_local_index == 1
+                @test vec.last_local_index == 4
+
+                @test vec.global_to_local_map == Dict{Int32, Int32}(6 => 5, 7 => 6)
+
+                @test vec.sized
+
+                # # Test individual assignment
+                vec[2] = 2.6
+
+                assemble!(vec)
+
+                @test size(vec) == 8
+
+                @test vec[2] == 2.6
+
+                @test vec[6] == 1.2
+
+                vec[3] = 8.7
+
+                assemble!(vec)
+
+                @test vec[3] == 8.7
+                @test vec[7] == 2.9
+            else
+                vec = GhostedPetscVec([1,2], n_local=(Int32)(4))
+
+                @test vec.first_local_index == 5
+                @test vec.last_local_index == 8
+
+                @test vec.global_to_local_map == Dict{Int32, Int32}(1 => 5, 2 => 6)
+
+                @test vec.sized
+
+                # # Test individual assignment
+                vec[6] = 1.2
+
+                assemble!(vec)
+
+                @test size(vec) == 8
+
+                @test vec[6] == 1.2
+
+                @test vec[2] == 2.6
+
+                vec[7] = 2.9
+
+                assemble!(vec)
+
+                @test vec[7] == 2.9
+            end
+
+            if MPI.Comm_rank(MPI.COMM_WORLD) == 0
+                vec = GhostedPetscVec([6,7], n_local=(Int32)(4))
+
+                # Test Ranged assignment
+                vec[1:2] = (Float64)[1 2]
+
+                assemble!(vec)
+
+                @test vec[1:2] == (Float64)[1,2]
+
+                # Test Array access to ghosts
+                @test vec[[1,6]] == [1,6]
+            else
+                vec = GhostedPetscVec([1,2], n_local=(Int32)(4))
+
+                # Test Ranged assignment
+                vec[6:7] = (Float64)[6 7]
+
+                assemble!(vec)
+
+                @test vec[6:7] == (Float64)[6,7]
+            end
+
+            if MPI.Comm_rank(MPI.COMM_WORLD) == 0
+                vec = GhostedPetscVec([6,7], n_local=(Int32)(4))
+
+                # Test Array assignment
+                vec[[1,3]] = (Float64)[1 3]
+
+                assemble!(vec)
+
+                @test vec[[1,3]] == (Float64)[1,3]
+            else
+                vec = GhostedPetscVec([1,2], n_local=(Int32)(4))
+
+                # Test Array assignment
+                vec[[6,8]] = (Float64)[6 8]
+
+                assemble!(vec)
+
+                @test vec[[6,8]] == (Float64)[6,8]
+            end
 
             # Test copy!
             if MPI.Comm_rank(MPI.COMM_WORLD) == 0
